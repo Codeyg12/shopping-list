512e9ebac53d502653c4f671a772751aeea4d35b
Codeyg12 <codey.gallup@gmail.com>
2023-05-13 15:44:43 -0600

    starting for all files

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputField = document.querySelector('#input-field')
+const addButton = document.querySelector('#add-button')
+
+addButton.addEventListener('click', () => {
+    console.log(inputField.value)
+})
