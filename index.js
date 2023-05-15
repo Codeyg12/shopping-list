0819a5558ff9ffc157f43933a7574de9ac3f867a
Codeyg12 <codey.gallup@gmail.com>
2023-05-15 16:21:37 -0600

    fine tuning database

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 const appSettings = {
-    databaseURL: "https://playground-ea176-default-rtdb.firebaseio.com/"
+    databaseURL: "https://realtime-databse-6ecea-default-rtdb.firebaseio.com/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, 'shoppingList')
 
 const inputField = document.querySelector('#input-field')
 const addButton = document.querySelector('#add-button')
 
 addButton.addEventListener('click', () => {
     let inputVal = inputField.value
-    console.log(inputVal)
+    
+    push(shoppingListInDB, inputVal)
+
+    console.log(`${inputVal} added to database`)
 })
