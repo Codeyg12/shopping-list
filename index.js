7b34ee4f47490ac74a81cff2d162feae01bfacd0
Codeyg12 <codey.gallup@gmail.com>
2023-05-15 15:53:26 -0600

    adding database

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
+
+const appSettings = {
+    databaseURL: "https://playground-ea176-default-rtdb.firebaseio.com/"
+}
+
 const inputField = document.querySelector('#input-field')
 const addButton = document.querySelector('#add-button')
 
