aba8c934c108c4a5c0ad6f1b8458c024a9e3dbe8
Codeyg12 <codey.gallup@gmail.com>
2023-05-15 17:02:09 -0600

    daily formatting

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://realtime-databse-6ecea-default-rtdb.firebaseio.com/"
-}
+  databaseURL: "https://realtime-databse-6ecea-default-rtdb.firebaseio.com/",
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, 'shoppingList')
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
-const inputField = document.querySelector('#input-field')
-const addButton = document.querySelector('#add-button')
-const shoppingList = document.querySelector('#shopping-list')
+const inputField = document.querySelector("#input-field");
+const addButton = document.querySelector("#add-button");
+const shoppingList = document.querySelector("#shopping-list");
 
-addButton.addEventListener('click', () => {
-    let inputVal = inputField.value
-    
-    // push(shoppingListInDB, inputVal)
+addButton.addEventListener("click", () => {
+  let inputVal = inputField.value;
 
-    appendToList(inputVal)
+  // push(shoppingListInDB, inputVal)
 
-    clearInput()
+  appendToShoppingList(inputVal);
 
-    console.log(`${inputVal} added to database`)
-})
+  clearInput();
 
-function appendToList(input) {
-    shoppingList.innerHTML += `<li>${input}</li>`
+  console.log(`${inputVal} added to database`);
+});
+
+function appendToShoppingList(input) {
+  shoppingList.innerHTML += `<li>${input}</li>`;
 }
 
 function clearInput() {
-    inputField.value = ""
+  inputField.value = "";
 }
